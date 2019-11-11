8d1a45212bc47d8ca616f783d93bf1c5d808bba2
Egor <trottimus.2001@mail.ru>
2019-11-11 20:12:21 +0200

    v1.0

---
+++ 4 и 5 Задания Блокчейн.py
@@ -0,0 +1,83 @@
+#Задание 4
+a = 281
+c = 624
+d = 970
+
+
+#Задание 5
+one = "00da296998d702dccef2b0d69c0413c6796764e60d27fb870f176cb73139be84"     # в "..." вставляем hash, который уже дан по заданию
+two = 723     # пишим число, которое дано по заданию
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Ничего не тронгай ниже
+import hashlib
+x = 1
+b = 1
+print("Задание 4:")
+o = str(hashlib.sha256(str(a).encode()).hexdigest())
+print(o)
+h = o + str(hashlib.sha256(str(c).encode()).hexdigest())
+o = str(hashlib.sha256(str(h).encode()).hexdigest())
+print(o)
+j = o + str(hashlib.sha256(str(d).encode()).hexdigest())
+o = str(hashlib.sha256(str(j).encode()).hexdigest())
+print(o)
+two1 = hashlib.sha256(str(two).encode()).hexdigest()
+string = one + two1
+while x != 1001:
+    s = hashlib.sha256(str(b).encode()).hexdigest()
+    gg = string
+    gg +=s
+    gg = hashlib.sha256(str(gg).encode()).hexdigest()
+    if gg[:2] == "00":
+        i = gg
+    x +=1
+    b +=1
+print()
+print("Задание 5:")
+print("В ответ пишешь: ", i)
