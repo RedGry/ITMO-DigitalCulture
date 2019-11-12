d885bcdec90d560cb552a388570c36d70a155fdf
Egor <trottimus.2001@mail.ru>
2019-11-12 19:11:10 +0200

    v1.1
    1) Updated files *.py
    2) In the code "Информационная безопасность.py" replaced a bunch elif (-s) on function chr() , now the code looks a bit better ( thx @foryourselfand )
    3) Added file for section "Сетевые технологии"

--- 4 и 5 Задания Блокчейн.py
+++ 4 и 5 Задания Блокчейн.py
@@ -16,24 +16,24 @@ x = 1
 b = 1
 print("Задание 4:")
 o = str(hashlib.sha256(str(a).encode()).hexdigest())
-print(o)
+print("1:",o)
 h = o + str(hashlib.sha256(str(c).encode()).hexdigest())
 o = str(hashlib.sha256(str(h).encode()).hexdigest())
-print(o)
+print("2:",o)
 j = o + str(hashlib.sha256(str(d).encode()).hexdigest())
 o = str(hashlib.sha256(str(j).encode()).hexdigest())
-print(o)
+print("3:",o)
 two1 = hashlib.sha256(str(two).encode()).hexdigest()
 string = one + two1
 while x != 1001:
     s = hashlib.sha256(str(b).encode()).hexdigest()
     gg = string
-    gg +=s
+    gg += s
     gg = hashlib.sha256(str(gg).encode()).hexdigest()
     if gg[:2] == "00":
         i = gg
-    x +=1
-    b +=1
+    x += 1
+    b += 1
 print()
 print("Задание 5:")
-print("В ответ пишешь: ", i)
+print("Пишешь в ответ:", i)
